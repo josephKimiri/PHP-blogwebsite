e8eb25b8880178043a3557494f5a2a7221481b70
josephKimiri <joseph.kimiri.macharia@gmail.com>
2022-02-22 16:07:56 +0300

    Change conn to con.

--- database/db_connect.php
+++ database/db_connect.php
@@ -16,5 +16,5 @@ $cleardb_db = substr($cleardb_url["path"],1);
 $active_group = 'default';
 $query_builder = TRUE;
 // Connect to DB
-$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+$con = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
  ?>
