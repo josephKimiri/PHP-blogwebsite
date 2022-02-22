24b57e69ab30ddd58e1203bc5d89981c2199c754
josephKimiri <joseph.kimiri.macharia@gmail.com>
2022-02-22 16:04:25 +0300

    Add heroku servers for remote deployment.

--- database/db_connect.php
+++ database/db_connect.php
@@ -1,8 +1,20 @@
 <?php
+/*** 
 $con=mysqli_connect("localhost","root","","blog_admin_db");
 // Check connection
 if (mysqli_connect_errno())
   {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
   }
+
+  */
+  $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
  ?>
