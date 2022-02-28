b2213b0658a420e09c3cc4cd1801d75b9343f1ab
josephKimiri <joseph.kimiri.macharia@gmail.com>
2022-02-28 10:42:58 +0300

    Add a script to display errors on Error 500.

--- blogadmin/membership_signup.php
+++ blogadmin/membership_signup.php
@@ -1,11 +1,11 @@
 <?php
 
 	$app_name = 'TECHILLL BLOG ADMIN';
-	
-	include("defaultLang.php");
-	include("language.php");
-	include("lib.php");
-	include_once("header.php");
+	$currDir = dirname(__FILE__);
+	include("{$currDir}/defaultLang.php");
+	include("{$currDir}/language.php");
+	include("{$currDir}/lib.php");
+	include_once("{$currDir}/header.php");
 
 	$adminConfig = config('adminConfig');
 
